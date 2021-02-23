6d5e50aa6eabff9c26edcdc982d6b226827f8055
Andrea Boschin <codeblock@gmail.com>
2021-02-24 00:24:38 +0100

    restriction utils

--- Elite.Repositories/Elite.Repositories.Abstractions/Criterias/RestrictionCriteria.cs
+++ Elite.Repositories/Elite.Repositories.Abstractions/Criterias/RestrictionCriteria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,21 @@ namespace Elite.Repositories.Abstractions.Criterias
         public string RestrictionTemplate { get; set; }
 
         public object[] Arguments { get; set; }
+
+        public static RestrictionCriteria FromQuerySet(IEnumerable<string> set, params object[] arguments)
+        {
+            var zipped = set
+                .Zip(arguments)
+                .Where(_ => _.Second != null);
+
+            var items = zipped
+                .Select((o, i) => $"({o.First.Replace("??", $"@{i}")}) ");
+
+            return new RestrictionCriteria
+            {
+                RestrictionTemplate = string.Join("&& ", items).Trim(),
+                Arguments = zipped.Select(_ => _.Second).ToArray(),
+            };
+        }
     }
 }
