dd22c9921daf946c2812b9c85fb0b72a38bd5b27
Andrea Boschin <codeblock@gmail.com>
2021-02-23 23:45:05 +0100

    added utilities for paging

---
+++ Elite.Repositories/Elite.Repositories.Abstractions/Criterias/GridCriteria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elite.Repositories.Abstractions.Criterias
+{
+    public class GridCriteria
+    {
+        public string[] Columns { get; set; }
+        public PagingCriteria Paging { get; set; }
+        public SortingCriteria Sorting { get; set; }
+
+        public string Projection()
+        {
+            return $"new({string.Join(',', this.Columns)})";
+        }
+    }
+}
